c75121630c4d0233516ed4000c0fb186e27231be
Hitarth <hitarthchudgar@gmail.com>
2021-03-05 23:41:08 -0500

    Handling click events

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const link = "http://www.google.com";
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('This button was clicked', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ', name, e.target);
+    }
+
     return ( 
         <div className="Home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Show More</button>
+            <button onClick={(e) => {
+                handleClickAgain('Hitarth', e);
+            }}>Show Name</button>
         </div>
      );
 }
