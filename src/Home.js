dcad9679b9a03068dbd57dbff71edb76564a12e0
Hitarth <hitarthchudgar@gmail.com>
2021-03-11 01:31:30 -0500

    Outputting lists

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('Hitarth');
-    const [age, setAge] = useState(20);
-
-    const handleClick = () =>{
-        setName('Hitu');
-        setAge(21);
-    }
+    const [blog, setBogs] = useState([
+        //id is used to reference
+        {title: 'UX Website', body: 'lorem ipsum ...', author: 'Hitarth', id: 1},
+        {title: 'Dev Website', body: 'lorem ipsum ...', author: 'Eric', id: 2},
+        {title: 'Research Website', body: 'lorem ipsum ...', author: 'Abhishek', id: 3},
+    ]);
     return ( 
         <div className="Home">
-            <h2>Homepage</h2>
-            <p>Hi I am { name } and am { age } years old!</p>
-            <button onClick={handleClick}>Show Name and Age</button>
+            {blog.map((blog) => (
+                <div className="Blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
